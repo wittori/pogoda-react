66f458beb6815e179dd2c74297f9c5f4813ae385
wittori <wittoria@ukr.net>
2023-04-05 17:26:03 +0300

    Add axios, bootstrap, css,js

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import Weather from "./Weather";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <Weather />
+        <footer>
+          Coded by Viktoria Fedash {""}
+          <a
+            href="https://github.com/wittori/weather-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+          {""} and {""}
+          <a
+            href="https://candid-beignet-efde6d.netlify.app/"
+            target="-blank"
+            rel="noreferrer"
+          >
+            {" "}
+            hosted on Netlify{" "}
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
+}
+
+export default App;
