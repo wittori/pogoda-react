1844d1ddc96becfe2736c517bc3988a6c579c9b7
wittori <wittoria@ukr.net>
2023-04-08 22:42:33 +0300

    Add date, defaultCity

--- src/Weather.js
+++ src/Weather.js
@@ -1,17 +1,18 @@
 import React, { useState } from "react";
 import axios from "axios";
 
+import FormattedDate from "./FormattedDate";
 import "./Weather.css";
 
 export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ loaded: false });
   const [city, setCity] = useState(props.defaultCity);
-  const [loaded, setLoaded] = useState(false);
-  const [weather, setWeather] = useState({});
 
   function displayWeather(response) {
-    console.log(response.data);
-    setLoaded(true);
-    setWeather({
+    setWeatherData({
+      loaded: true,
+      city: response.data.name,
+      date: new Date(response.data.dt * 1000),
       temperature: response.data.main.temp,
       wind: response.data.wind.speed,
       humidity: response.data.main.humidity,
@@ -23,26 +24,34 @@ export default function Weather(props) {
 
   function handleSubmit(event) {
     event.preventDefault();
+    searchCity();
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
+  function searchCity() {
     let apiKey = "3a94f3778290bfeee61278505dbbe51d";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(displayWeather);
   }
-  function updateCity(event) {
-    event.preventDefault();
-    setCity(event.target.value);
-  }
-  if (loaded) {
+
+  if (weatherData.loaded) {
     return (
       <div className="Weather">
         <div className="container p-4">
           <div className="row">
             <div className="col-md-6">
-              <h1>{city}</h1>
+              <h1>{weatherData.city}</h1>
               <h2>
-                <span>{weather.date}</span>
+                <span>
+                  {" "}
+                  <FormattedDate date={weatherData.date} />
+                </span>
               </h2>
               <br />
-              <h3>{weather.description}</h3>
+              <h3>{weatherData.description}</h3>
               <br />
               <button type="submit" className="currentLocationButton">
                 Current location
@@ -74,7 +83,7 @@ export default function Weather(props) {
                 <div className="col-6 weatherNow">
                   <ul>
                     <li className="tempNow">
-                      <strong>{Math.round(weather.temperature)}</strong>
+                      <strong>{Math.round(weatherData.temperature)}</strong>
                       <a href="/" className="celsiusTemp">
                         °C
                       </a>
@@ -84,16 +93,18 @@ export default function Weather(props) {
                       </a>
                     </li>
                     <li className="feelsLike">
-                      Feels like: {Math.round(weather.feels_like)} °C
+                      Feels like: {Math.round(weatherData.feels_like)} °C
                     </li>
-                    <li className="humidity">Humidity: {weather.humidity} %</li>
-                    <li className="wind">Wind: {weather.wind} km/h</li>
+                    <li className="humidity">
+                      Humidity: {weatherData.humidity} %
+                    </li>
+                    <li className="wind">Wind: {weatherData.wind} km/h</li>
                   </ul>
                 </div>
                 <div className="col-6 ">
                   <img
-                    src={weather.icon}
-                    alt={weather.description}
+                    src={weatherData.icon}
+                    alt={weatherData.description}
                     className="mainWeatherPic img-fluid mt-5"
                   />
                 </div>
@@ -104,25 +115,7 @@ export default function Weather(props) {
       </div>
     );
   } else {
-    return (
-      <div className="Weather">
-        <form onSubmit={handleSubmit}>
-          <div className="row">
-            <div className="col-9">
-              <input
-                type="search"
-                placeholder="Enter a city"
-                className="form-control"
-                autoFocus="on"
-                onChange={updateCity}
-              />
-            </div>
-            <div className="col-3">
-              <input type="submit" value="Search" className="search-button" />
-            </div>
-          </div>
-        </form>
-      </div>
-    );
+    searchCity();
+    return "Loading";
   }
 }
