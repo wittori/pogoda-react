67abe06027444cd928e70d10a6ffba31a929e5d0
wittori <wittoria@ukr.net>
2023-04-17 14:53:31 +0300

    Add forecast for 1 day

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather defaultCity="New York" />
+        <Weather defaultCity="Kyiv" />
         <footer>
           Coded by Viktoria Fedash {""}
           <a
--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,21 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
-import WeatherIcon from "./WeatherIcon";
+import WeatherForecastDay from "./WeatherForecastDay";
 import "./WeatherForecast.css";
 
-export default function WeatherForecast() {
-  return (
-    <div className="WeatherForecast">
-      <div className="row">
-        <div className="col">
-          <div className="WeatherForecast-day">WED</div>
-          <WeatherIcon code="01d" size={70} />
-          <div className="WeatherForecast-temperatures">
-            <span className="WeatherForecast-temperature-max">21°</span>
-            <span className="WeatherForecast-temperature-min">19°</span>
+export default function WeatherForecast(props) {
+  let [loaded, setLoaded] = useState(false);
+  let [forecast, setForecast] = useState(null);
+
+  function handleResponse(response) {
+    console.log(response.data.daily);
+    setForecast(response.data.daily);
+    setLoaded(true);
+  }
+
+  if (loaded) {
+    return (
+      <div className="WeatherForecast">
+        <div className="row">
+          <div className="col">
+            <WeatherForecastDay forcastData={forecast[0]} />
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    let longitude = props.coordinates.lon;
+    let latitude = props.coordinates.lat;
+    const apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+    return null;
+  }
 }
